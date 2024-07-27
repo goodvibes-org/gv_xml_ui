58f5c590ff1b056ccf1ec72bab61218daff2de07
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2024-07-27 17:30:28 -0300

    cierro comit

---
+++ validacion/sin_correccIon_pos.py
@@ -0,0 +1,38 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+EDU_DF_PATH = "scores_edu_sin_correccion.csv"
+R_DF_PATH = "scores_r.csv"
+
+edu = pd.read_csv(EDU_DF_PATH)
+r = pd.read_csv(R_DF_PATH, sep = "\t")
+
+
+# Acá asumo que `norm.HPC.score` es el 10 + score
+join = edu.set_index("codigo").join(r.set_index("Codigo"), lsuffix = "edu",rsuffix= "r")
+join["norm.HPC.score"] = join["norm.HPC.score"].apply( lambda x :  10 - x)
+join["diferencia"] = join["score"] - join["norm.HPC.score"]
+df = join[[ "name", "score", "norm.HPC.score", "diferencia", "numero_ingredientes"]]
+df.to_csv("comparacion_sin_corr.csv")
+
+fig, ax = plt.subplots(nrows = 1, ncols = 3, figsize = (40, 10))
+join.to_csv("datos_todos_sin_corr.csv")
+ax[0].hist(df.diferencia, bins = 50)
+ax[0].set_xlabel("Diferencia entre puntaje Edu - R")
+ax[0].set_ylabel("Instancias")
+# plt.savefig("diferencias.png")
+# plt.show()
+ax[1].hist(df.score, bins = 50 , alpha = .5, label= "Edu")
+ax[1].hist(df["norm.HPC.score"], bins = 50 , alpha = .5, label = "R")
+ax[1].legend()
+ax[1].set_xlabel("Puntaje")
+ax[1].set_ylabel("Instancias")
+
+ax[2].scatter(df.diferencia, df.numero_ingredientes)
+ax[2].set_xlabel("diferencia entre puntaje Edu - R")
+ax[2].set_ylabel("Cantidad de Ingredientes")
+
+
+plt.savefig("sin_correccion_puntajes.png")
+plt.show()
